a6862284c8b58479fdf58bbdcb5c257b3bd4c9ca
Prabin Amatya <pnamatya@gmail.com>
2015-10-27 20:47:56 -0500

    initial commit

---
+++ src/main/java/com/prabin/marshal/MarshallingApplication.java
@@ -0,0 +1,12 @@
+package com.prabin.marshal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarshallingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarshallingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/prabin/marshal/MarshallingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prabin.marshal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MarshallingApplication.class)
+public class MarshallingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
